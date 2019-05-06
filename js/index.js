380eb5969977c5cae18ecd61401a32804e4a1c91
ChillyBreeze1998 <1342327426@qq.com>
2019-05-06 20:52:56 +0800

    二级菜单

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(function () { 
+  $('.header .fr .wdjd').mousemove(function () { 
+     $('.wdjd .wdjd-pull-down').css('display','block');
+  });
+  $('.header .fr .wdjd').mouseout(function () { 
+    $('.wdjd .wdjd-pull-down').css('display','none');
+ });
+ });
